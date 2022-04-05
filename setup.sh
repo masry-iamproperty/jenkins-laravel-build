25f5ba7635b5a9a8cd0db936faa75d0d6b278b5c
masry-iamproperty <mohammed.almasry@iamproperty.com>
2022-04-05 13:17:28 +0200

    add setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+echo "Building application"
 
-echo "We are printing this meessage only for funzzies"
+composer install -vvv
+
+if [! -f .env ]; then
+    echo ".env file not found ... generating .env file"
+    cp .env.example .env
+fi
+
+php artisan key:generate
