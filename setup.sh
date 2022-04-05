788832dc0ab5139b471fed2d82f3010f1428535c
masry-iamproperty <mohammed.almasry@iamproperty.com>
2022-04-05 13:20:12 +0200

    rectify bash syntax

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Building application"
 
 composer install -vvv
 
-if [! -f .env ]; then
+if [ ! -f .env ]; then
     echo ".env file not found ... generating .env file"
     cp .env.example .env
 fi
